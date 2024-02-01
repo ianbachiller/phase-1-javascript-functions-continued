4834173b5afa4c7264146eede607a1a709bab8ef
Ian Bachiller <ianbachise@gmail.com>
2024-02-01 01:50:46 -0500

    complete js functions continued scope, innerfunctions, etc. lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+function saturdayFun(activity){
+    // const a = `This Saturday, I want to `
+    if(activity === null || activity === undefined){
+        switch (activity){
+            case activity:
+                return 'This Saturday, I want to roller-skate!';
+        }
+    }
+    else{
+        return `This Saturday, I want to ${activity}!`;
+    }
+}
+saturdayFun(activity)
+
+function mondayWork(activity){
+    if(activity === null || activity === undefined){
+        switch (activity){
+            case activity:
+                return 'This Monday, I will go to the office.';
+        }
+    }
+    else{
+        return `This Monday, I will work from home.`;
+    }
+}
+mondayWork(activity)
+
+function wrapAdjective(string = "*"){
+    const innerF = function (def = "special"){
+        return `You are ${string}${def}${string}!`
+    }
+   return innerF 
+}
